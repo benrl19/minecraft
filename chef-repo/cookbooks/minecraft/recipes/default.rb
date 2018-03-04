3d5e74a0b5498e5d581d585961d75f5d0c61842c
Ubuntu <ubuntu@vps521033.ovh.net>
2018-03-04 02:23:22 +0100

    first commit

---
+++ chef-repo/cookbooks/minecraft/recipes/default.rb
@@ -0,0 +1,64 @@
+execute "apt-get update" do
+    command "apt-get update"
+end
+
+package "openjdk-9-jre-headless" do
+    action :install
+end
+
+user 'minecraft' do
+ system true
+ shell '/bin/nologin'
+ action :create           
+end
+
+directory '/var/minecraft' do
+ owner 'minecraft'
+ group 'minecraft'
+ mode '0775'
+ action :create
+end
+
+cookbook_file "/var/minecraft/eula.txt" do
+    source "eula.txt"
+    owner 'minecraft'
+    group 'minecraft'
+    mode "0644"
+    action :create
+end
+
+cookbook_file "/var/minecraft/server.properties" do
+    source "server.properties"
+    owner 'minecraft'
+    group 'minecraft'
+    mode "0644"
+    action :create
+end
+
+cookbook_file "/var/minecraft/ops.txt" do
+   source "admin.txt"
+   owner 'minecraft'
+   group 'minecraft'
+   mode "0644"
+   action :create
+end
+
+remote_file  "/var/minecraft/minecraft_server.1.12.2.jar" do
+    source "https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar"
+    owner 'minecraft'
+    group 'minecraft'
+    mode '0775'
+    action :create
+end
+
+cookbook_file "/lib/systemd/system/minecraft.service" do
+    source "minecraftservice"
+    owner 'root'
+    group 'root'
+    mode "0755"
+    action :create
+end
+
+service "minecraft" do
+    action [:start, :enable, :restart]
+end
---
+++ solo.rb
@@ -0,0 +1,2 @@
+cookbook_path '/home/ubuntu/minecraft/chef-repo/cookbooks'
+role_path '/home/ubuntu/minecraft/chef-repo/roles'
