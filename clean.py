2d0b980facfb6875ccc85a403504254224655bc0
Ben Lucas <benlucas@beggars.com>
2018-05-05 13:30:10 +0100

    Set clean.py to use dropbox upload inputs

--- clean.py
+++ clean.py
@@ -3,18 +3,22 @@ import subprocess
 
 folder = []
 
-with open('fileoutput.txt') as file:
-    for line in file:
-        if line[:4] == ' [D]':
-            date = line.replace(' [D]  ','')
-            date = date.replace('\n','')
-            folder.append(date)
-            
+files =  subprocess.Popen(['/var/backup_minecraft/dropbox_uploader.sh','list'],stdout=subprocess.PIPE)
+outputbytes = files.stdout.read()
+output = outputbytes.decode("utf-8")
+
+
+for line in output.split("\n"):
+    if line[:4] == ' [D]':
+        date = line.replace(' [D]  ','')
+        date = date.replace('\n','')
+        folder.append(date)
+
 folder.sort(key=int)
 keepers = folder[-5:]
-print(keepers)
+print("We are keeping: " + str(keepers))
 goners = list(set(folder) - set(keepers))
-print(goners)
+print("We are deleting: " + str(goners))
 
 for i in goners:
     subprocess.call(['/var/backup_minecraft/dropbox_uploader.sh','delete',i])
